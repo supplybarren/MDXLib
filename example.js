29bcd2cdf08946fe3a182828c4eaff4d2887a7bb
moly <68660296+act-molly@users.noreply.github.com>
2020-07-22 14:33:56 -0700

    Adding MDXsaveconfig & MDXloadconfig

--- example.js
+++ example.js
@@ -20,53 +20,61 @@ var ag = 10;
 var ab = 40;
 var aa = 255;
 
-function cleartabs(){
-    tab1 = false;
-    tab2 = false;
-}
+var tabgy = MDX.agy + 30;
+var myTab1 = new MDX.MDXTab("tab1", true, MDX.agx + 10, tabgy);
+var myTab2 = new MDX.MDXTab("tab2", false, MDX.agx + 10, tabgy + 20);
 
-function main(){
-    var tabgy = MDX.agy + 30;
+function main() {
     var sx = MDX.agx + 143;
     var sy = tabgy;
 
     MDX.menu("MDX", "GUI", MDX.agx, MDX.agy, 500, 300);
-    if (MDX.drag(MDX.agx, MDX.agy).x != 200 || MDX.drag(MDX.agx, MDX.agy).y != 200){
+    if (MDX.drag(MDX.agx, MDX.agy).x != 200 || MDX.drag(MDX.agx, MDX.agy).y != 200) {
         MDX.agx = MDX.drag(MDX.agx, MDX.agy).x - 150;
         MDX.agy = MDX.drag(MDX.agx, MDX.agy).y - 10;
     }
 
-    if (MDX.tab("tab 1", MDX.agx + 10, tabgy)){
-        cleartabs();
-        tab1 = true;
-    }
+    MDX.tab(myTab1);
+    MDX.tab(myTab2);
 
-    if (MDX.tab("tab 2", MDX.agx + 10, tabgy + 20)){
-        cleartabs();
-        tab2 = true;
-    }
-
-    if (tab1){
+    if (myTab1.getTabVisibility()) {
         if (MDX.checkbox("checkbox", sx, sy, checkboxvalue))
             checkboxvalue = !checkboxvalue;
 
         textboxstring = MDX.textbox("textbox", sx, sy + 15, textboxstring);
 
-        if (MDX.button("button", sx, sy + 50)){
-            Local.SetClanTag(textboxstring);
+        if (MDX.button("Save config", sx, sy + 50)){
+            var config = '{ "slidervalue":' + slidervalue + ', "verticalslidervalue":' + verticalslidervalue + ', "checkboxvalue":' + checkboxvalue + 
+                         ', "arrayopened":' + arrayopened + ', "chosenoption":' + chosenoption + ', "textboxstring":"' + textboxstring + '", "r":' + ar + 
+                         ', "g":' + ag + ', "b":' + ab + ', "a":' + aa + '}';
+            MDX.saveconfig(config);
+        }
+
+        if (MDX.button("Load config", sx, sy + 80)){
+            var cfg = JSON.parse(MDX.loadconfig());
+            slidervalue = cfg.slidervalue;
+            verticalslidervalue = cfg.verticalslidervalue;
+            checkboxvalue = cfg.checkboxvalue;
+            textboxstring = cfg.textboxstring;
+            chosenoption = cfg.chosenoption;
+            ar = cfg.r;
+            ag = cfg.g;
+            ab = cfg.b;
+            aa = cfg.a;
         }
 
         var max = 1;
-        verticalslidervalue = MDX.verticalslider("centered", sx, sy + 80, verticalslidervalue, max, true);
+        verticalslidervalue = MDX.verticalslider("centered", sx, sy + 110, verticalslidervalue, max, true);
         var finalValue = verticalslidervalue * max;
     }
 
-    if (tab2){
+    if (myTab2.getTabVisibility()) {
         slidervalue = MDX.slider("slider", sx, sy + 33, slidervalue, 1);
 
+
         var colorpicker = MDX.colorpicker("color picker", sx, sy + 63, ar, ag, ab, aa, colorpickeropened);
-        if (colorpicker != undefined){
-            if (colorpicker == "closed"){
+        if (colorpicker != undefined) {
+            if (colorpicker == "closed") {
                 colorpickeropened = !colorpickeropened;
             } else {
                 ar = colorpicker.r;
@@ -75,11 +83,11 @@ function main(){
                 aa = colorpicker.a;
             }
         }
-        
+
         var dropdownoptions = ["bruh", "ok", "lmao", "nice", "lmao", "test", "because i can"];
         var dropdown = MDX.dropdown("dropdown", sx, sy, dropdownoptions, arrayopened, dropdownoptions[chosenoption]);
-        if (dropdown != undefined){
-            if (dropdown == "closed"){
+        if (dropdown != undefined) {
+            if (dropdown == "closed") {
                 arrayopened = !arrayopened;
             } else {
                 chosenoption = dropdown;
