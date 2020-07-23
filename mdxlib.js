432afdfaca478ebeeea4e21532803aef22057f0b
moly <68660296+act-molly@users.noreply.github.com>
2020-07-22 17:48:00 -0700

    Update mdxlib.js

--- mdxlib.js
+++ mdxlib.js
@@ -6,6 +6,7 @@ var agx = 200;
 var agy = 200;
 var biggestwidth = 0;
 var globaltime = Globals.Realtime();
+var savedcolor = undefined;
 //#endregion
 
 //#region Javascript function definitions
@@ -23,8 +24,8 @@ if (!Object.entries) {
 //#endregion
 
 var Base64 = {
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    encode : function (input) {
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (input) {
         var output = "";
         var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
         var i = 0;
@@ -43,13 +44,13 @@ var Base64 = {
                 enc4 = 64;
             }
             output = output +
-            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-    
+                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
         }
         return output;
     },
-    decode : function (input) {
+    decode: function (input) {
         var output = "";
         var chr1, chr2, chr3;
         var enc1, enc2, enc3, enc4;
@@ -74,19 +75,17 @@ var Base64 = {
         output = Base64._utf8_decode(output);
         return output;
     },
-    _utf8_encode : function (string) {
-        string = string.replace(/\r\n/g,"\n");
+    _utf8_encode: function (string) {
+        string = string.replace(/\r\n/g, "\n");
         var utftext = "";
         for (var n = 0; n < string.length; n++) {
             var c = string.charCodeAt(n);
             if (c < 128) {
                 utftext += String.fromCharCode(c);
-            }
-            else if((c > 127) && (c < 2048)) {
+            } else if ((c > 127) && (c < 2048)) {
                 utftext += String.fromCharCode((c >> 6) | 192);
                 utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else {
+            } else {
                 utftext += String.fromCharCode((c >> 12) | 224);
                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
                 utftext += String.fromCharCode((c & 63) | 128);
@@ -94,30 +93,28 @@ var Base64 = {
         }
         return utftext;
     },
-    _utf8_decode : function (utftext) {
+    _utf8_decode: function (utftext) {
         var string = "";
         var i = 0;
         var c = c1 = c2 = 0;
-        while ( i < utftext.length ) {
+        while (i < utftext.length) {
             c = utftext.charCodeAt(i);
             if (c < 128) {
                 string += String.fromCharCode(c);
                 i++;
-            }
-            else if((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i+1);
+            } else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
                 i += 2;
-            }
-            else {
-                c2 = utftext.charCodeAt(i+1);
-                c3 = utftext.charCodeAt(i+2);
+            } else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
                 i += 3;
             }
         }
         return string;
-    } 
+    }
 }
 
 function MDXmenu(col_title, white_title, gx, gy, gwidth, gheight, bg_col_top, bg_col_bottom) {
@@ -460,8 +457,8 @@ function MDXtextbox(text, gx, gy, string) {
     var curPos = Input.GetCursorPosition();
     var curx = curPos[0];
     var cury = curPos[1];
-    var keyCodes = [0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A];
-    var keyChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var keyCodes = [0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x20];
+    var keyChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "];
     var font = Render.AddFont("Tahoma", 7, 700);
     var texty = gy;
     Render.GradientRect(gx, gy + 12, 90, 17, 0, [12, 12, 12, 255], [24, 24, 24, 255]);
@@ -520,22 +517,25 @@ function MDXcolorpicker(text, gx, gy, ar, ag, ab, aa, open) {
     if (open) {
         Render.Rect(gx - 3, gy + 9, 31, 21, [r, g, b, 255]);
         texty = gy - 2;
+        Render.FilledRect(gx - 2, gy + 35, 270, 110, [9, 9, 9, 255]);
+        Render.Rect(gx - 2, gy + 35, 270, 110, [27, 27, 27, 255]);
         Render.FilledRect(gx - 2, gy + 35, 270, 135, [9, 9, 9, 255]);
         Render.Rect(gx - 2, gy + 35, 270, 135, [27, 27, 27, 255]);
         returnr = MDXcolorslider("red", gx + 5, gy + 40, returnr, "red");
         returng = MDXcolorslider("green", gx + 5, gy + 65, returng, "green");
         returnb = MDXcolorslider("blue", gx + 5, gy + 90, returnb, "blue");
         returna = MDXcolorslider("alpha", gx + 5, gy + 115, returna, "alpha");
-        if (MDXbutton("Copy", gx + 5, gy + 145)){
-            var color = Base64.encode('{"r":' + returnr + ', "g":' + returng + ', "b":' + returnb + ', "a":' + returna + '}');
-            Cheat.ExecuteCommand("xbox_throttlebias " + color);
+        if (MDXbutton("Copy", gx + 5, gy + 145)) {
+            savedcolor = Base64.encode('{"r":' + returnr + ', "g":' + returng + ', "b":' + returnb + ', "a":' + returna + '}');
         }
-        if (MDXbutton("Paste", gx + 60, gy + 145)){
-            var savedcolor = JSON.parse(Base64.decode(Convar.GetString("xbox_throttlebias")));
-            returnr = savedcolor.r;
-            returng = savedcolor.g;
-            returnb = savedcolor.b;
-            returna = savedcolor.a;
+        if (MDXbutton("Paste", gx + 60, gy + 145)) {
+            if (savedcolor !== 'undefined') {
+                var sav_col = JSON.parse(Base64.decode(savedcolor));
+                returnr = sav_col.r;
+                returng = sav_col.g;
+                returnb = sav_col.b;
+                returna = sav_col.a;
+            }
         }
     }
     Render.StringCustom(gx, texty, 0, text, [255, 255, 255, 150], font);
@@ -546,7 +546,6 @@ function MDXcolorpicker(text, gx, gy, ar, ag, ab, aa, open) {
     Render.FilledRect(gx + 2, gy + 19, 10, 6, [255, 255, 255, 255]);
     Render.FilledRect(gx + 12, gy + 19, 11, 6, [214, 214, 214, 255]);
     Render.FilledRect(gx + 2, gy + 14, 21, 11, [ar, ag, ab, aa]);
-
     return {
         r: returnr,
         g: returng,
@@ -555,12 +554,12 @@ function MDXcolorpicker(text, gx, gy, ar, ag, ab, aa, open) {
     };
 }
 
-function MDXsaveconfig(json){
+function MDXsaveconfig(json) {
     var config = Base64.encode(json);
     Cheat.ExecuteCommand("xbox_throttlespoof " + config);
 }
 
-function MDXloadconfig(){
+function MDXloadconfig() {
     var config = Base64.decode(Convar.GetString("xbox_throttlespoof"));
     return config;
 }
